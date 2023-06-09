f51b667b6ea8aef3d7d0e14a0281f31e1038f6ee
quentin carrel <quentin.carrel@etu.univ-grenoble-alpes.fr>
2023-06-09 15:08:41 +0200

    [R] Standalone sweeper grid & stuff

--- lib/game.dart
+++ lib/game.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:demineur/modele.dart' as modele;
+import 'package:demineur/widgets/sweeper.dart' as sweeper;
 
 class GridScreen extends StatefulWidget {
   final modele.Grille grille;
@@ -18,9 +19,6 @@ class _GrilleDemineur extends State<GridScreen> {
   {
     final modele.Grille _grille = widget.grille;
     final Stopwatch timer = widget.timer;
-    int rowIM = 0;
-    int colIM = 0;
-    double size = 900/widget.grille.taille;
     bool isOver = _grille.isFinie();
     if(isOver){
         timer.stop();
@@ -33,24 +31,7 @@ class _GrilleDemineur extends State<GridScreen> {
         body: Row(
           // Ligne -> Colonnes -> Cases
           children: [
-            ..._grille.grille.map((row) {
-              colIM = 0;
-              var colObj = Column(children: [
-                ...row.map((dCase) {
-                  var cell =  Cell(
-                     updateParent: updateParent,
-                     grille:_grille,
-                     x:colIM,
-                     y:rowIM,
-                     dCase:dCase,
-                     size: size); 
-                  colIM++;
-                  return cell;
-                })
-              ]);
-              rowIM++;
-              return colObj;
-            }),
+            ...sweeper.makeGrid(_grille, updateParent),
             Text(messageEtat(_grille)),
             Text(timer.elapsed.toString()),
             Builder(
@@ -64,7 +45,7 @@ class _GrilleDemineur extends State<GridScreen> {
                 else{
                     return OutlinedButton(
                       onPressed: () => {},
-                      child: const Text('aaaaaaa')
+                      child: const Text('Keep on playing')
                     );
                 }
               }
@@ -91,82 +72,5 @@ class _GrilleDemineur extends State<GridScreen> {
          grille.mettreAJour(modele.Coup(y, x, action)),
      });
 
-  }
-}
-
-class Cell extends StatelessWidget {
-
-    final void Function(modele.Grille, int, int, modele.Action) updateParent;
-    final modele.Grille grille;
-    final int x;
-    final int y;
-    final double size;
-    final modele.Case dCase;
-
-    const Cell({
-        super.key,
-        required this.updateParent,
-        required this.grille,
-        required this.x,
-        required this.y,
-        required this.dCase,
-        required this.size,
-    });
-
-  @override
-  Widget build(BuildContext context) {
-      var btn = SizedBox(
-          width: size,
-          height: size,
-          child: OutlinedButton(
-            onPressed: () => updateParent(grille, x, y, modele.Action.decouvrir),
-            onLongPress: () => updateParent(grille, x, y, modele.Action.marquer),
-            style: ButtonStyle(
-              backgroundColor: MaterialStatePropertyAll(caseToColor(dCase)),
-              foregroundColor: const MaterialStatePropertyAll(Colors.white),
-              padding: const MaterialStatePropertyAll(EdgeInsets.zero), // and this
-            ),
-            child: Align(
-                alignment: Alignment.center,
-                child: Text(
-                    caseToText(dCase, false),
-                    style: TextStyle(fontSize: size/2.5),
-                    textAlign: TextAlign.center,
-                )
-            ),
-          ),
-      );
-
-      return btn;
-  }
-
-  // "*" If undiscovered, "#nb" if there are any, nothing otherwise
-  String caseToText(modele.Case laCase, bool isFini)
-  {
-    if(laCase.marquee){
-      return "⚑";
-    }
-    if(!laCase.decouverte){
-      return "*";
-    }
-    if(laCase.minee) {
-      return "💣";
-    } else if(laCase.nbMinesAutour > 0) {
-      return laCase.nbMinesAutour.toString();
-    } else {
-      return "";
-    }
-  }
-
-  // Orange if marked, blue is undiscovered, grey otherwise
-  Color caseToColor(modele.Case laCase)
-  {
-    if(laCase.marquee) {
-      return Colors.orange;
-    } else if(!laCase.decouverte) {
-      return Colors.blue;
-    } else {
-      return Colors.grey;
-    }
   }
 }
---
+++ lib/widgets/sweeper.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:demineur/modele.dart' as modele;
+
+List makeGrid(modele.Grille grille,updateParent) {
+  List rows;
+
+  int colIM = 0;
+  int rowIM = 0;
+  double size = 750/grille.taille;
+
+  // For each row, iter over its elems
+  rows = grille.grille.map((row) {
+    colIM = 0;
+    var column = Column(children: [
+    // For each column in the row, generate a cell
+    ...row.map((dCase) {
+      var cell = Cell(
+           updateParent: updateParent,
+           grille:grille,
+           x:colIM,
+           y:rowIM,
+           dCase:dCase,
+           size: size); 
+      colIM++;
+      return cell;
+      })
+    ]);
+    rowIM++;
+    return column;
+  }).toList();
+
+  return rows;
+}
+
+class Cell extends StatelessWidget {
+
+  final void Function(modele.Grille, int, int, modele.Action) updateParent;
+  final modele.Grille grille;
+  final int x;
+  final int y;
+  final double size;
+  final modele.Case dCase;
+
+  const Cell({
+    super.key,
+    required this.updateParent,
+    required this.grille,
+    required this.x,
+    required this.y,
+    required this.dCase,
+    required this.size,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var btn = SizedBox(
+      width: size,
+      height: size,
+      child: OutlinedButton(
+      onPressed: () => updateParent(grille, x, y, modele.Action.decouvrir),
+      onLongPress: () => updateParent(grille, x, y, modele.Action.marquer),
+      style: ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll(caseToColor(dCase)),
+        foregroundColor: const MaterialStatePropertyAll(Colors.white),
+        padding: const MaterialStatePropertyAll(EdgeInsets.zero), // and this
+      ),
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(
+          caseToText(dCase, false),
+          style: TextStyle(fontSize: size/2.5),
+          textAlign: TextAlign.center,
+        )
+      ),
+      ),
+    );
+
+    return btn;
+  }
+
+  // "*" If undiscovered, "#nb" if there are any, nothing otherwise
+  String caseToText(modele.Case laCase, bool isFini)
+  {
+    if(laCase.marquee){
+      return "⚑";
+    }
+    if(!laCase.decouverte){
+      return "*";
+    }
+    if(laCase.minee) {
+      return "💣";
+    } else if(laCase.nbMinesAutour > 0) {
+      return laCase.nbMinesAutour.toString();
+    } else {
+      return "";
+    }
+  }
+
+  // Orange if marked, blue is undiscovered, grey otherwise
+  Color caseToColor(modele.Case laCase)
+  {
+    if(laCase.marquee) {
+      return Colors.orange;
+    } else if(!laCase.decouverte) {
+      return Colors.blue;
+    } else {
+      return Colors.grey;
+    }
+  }
+}
