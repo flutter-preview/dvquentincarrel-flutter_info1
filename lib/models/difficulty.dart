ace6dafc38dafec58dddb6bb1f436c911db5dcc1
quentin carrel <quentin.carrel@etu.univ-grenoble-alpes.fr>
2023-06-21 14:56:28 +0200

    [A] difficulty model

---
+++ lib/models/difficulty.dart
@@ -0,0 +1,25 @@
+enum Level {easy, medium, hard}
+
+class Difficulty {
+  int taille = 0;
+  int nbMines = 0;
+  Level level = Level.easy;
+
+  Difficulty(Level level) {
+    switch(level){
+      case Level.easy:
+        taille = 8;
+        nbMines = 6;
+      break;
+      case Level.medium:
+        taille = 15;
+        nbMines = 45;
+      break;
+      case Level.hard:
+        taille = 35;
+        nbMines = 300;
+      break;
+    }
+    level=level;
+  }
+}
